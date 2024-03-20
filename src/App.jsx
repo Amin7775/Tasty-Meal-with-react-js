0a1f2d669e6315f47c0ec9cfb00cd3ab7dbfa4fa
Amin7775 <amin.dpi24@gmail.com>
2024-03-20 17:29:19 +0600

    Changed Nav Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom"
 import Header from "./components/Header/Header"
-
+import './font.css'
 function Main() {
  
 
   return (
     <>
+    <div className="roboto">
       <Header></Header>
       <Outlet></Outlet>
+    </div>
     </>
   )
 }
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -3,18 +3,21 @@ import './navbar.css'
 
 const NavBar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-[#23180d] py-5">
+        <div className="container mx-auto">
+    
       <div className="flex-1">
-        <a className="btn btn-ghost text-2xl">Tasty Meals</a>
+        <a className="btn btn-ghost text-4xl text-[#ffa038]">Tasty Meals</a>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1 space-x-4 text-lg font-medium text-gray-600 mr-10" id="sidebar">
-          <NavLink to={'/home'}>Home</NavLink>
-          <NavLink to={'/menu'}>Menu</NavLink>
-          <NavLink to={'/aboutUs'}>About Us</NavLink>
-          <NavLink to={'/login'}>Login</NavLink>
+        <ul className="menu menu-horizontal px-1 space-x-4 text-lg text-white mr-10" id="sidebar">
+          <NavLink className={'bg-red-600 px-3 py-1 rounded-sm'} to={'/home'}>Home</NavLink>
+          <NavLink className={'bg-red-600 px-3 py-1 rounded-sm'} to={'/menu'}>Menu</NavLink>
+          <NavLink className={'bg-red-600 px-3 py-1 rounded-sm'} to={'/aboutUs'}>About Us</NavLink>
+          <NavLink className={'bg-red-600 px-3 py-1 rounded-sm'} to={'/login'}>Login</NavLink>
 
         </ul>
+      </div>
       </div>
     </div>
   );
